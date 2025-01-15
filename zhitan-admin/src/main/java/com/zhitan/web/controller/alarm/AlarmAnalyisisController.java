4b61ee152e5e23589c5bf95e5ba68f84aa80b963
ustcyc <yincun@163.com>
2025-01-15 23:01:08 +0800

    update description

--- zhitan-admin/src/main/java/com/zhitan/web/controller/alarm/AlarmAnalyisisController.java
+++ zhitan-admin/src/main/java/com/zhitan/web/controller/alarm/AlarmAnalyisisController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * description todu
+geng * description 报警分析相关功能
  *
  * @author hmj
  * @date 2024-10-26 17:31
