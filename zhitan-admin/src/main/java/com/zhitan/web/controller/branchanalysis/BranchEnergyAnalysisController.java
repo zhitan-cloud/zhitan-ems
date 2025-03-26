d76d11c557871d5f2e3319a255710bdcf12a21f9
zt916916 <1903964620@qq.com>
2025-03-26 10:11:59 +0800

    Revert "支路用能分析"
    This reverts commit d6fb5de9

---
+++ zhitan-admin/src/main/java/com/zhitan/web/controller/branchanalysis/BranchEnergyAnalysisController.java
@@ -0,0 +1,37 @@
+package com.zhitan.web.controller.branchanalysis;
+
+import com.zhitan.branchanalysis.domain.BranchAnalysisVO;
+import com.zhitan.common.core.controller.BaseController;
+import com.zhitan.common.core.domain.AjaxResult;
+import com.zhitan.branchanalysis.service.IBranchAnalysisService;
+
+import com.zhitan.realtimedata.domain.dto.BranchAnalysisDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *支路用能分析
+ *
+ * @author zt
+ * @date 2021-01-11
+ */
+@RestController
+@RequestMapping("/branchanalysis")
+@Api(value = "支路用能能耗统计",tags = {"支路用能分析"})
+public class BranchEnergyAnalysisController extends BaseController {
+
+    @Autowired
+    private IBranchAnalysisService branchAnalysisService;
+
+    @GetMapping("/list")
+    @ApiOperation(value = "支路用能能耗统计列表")
+    public AjaxResult list(BranchAnalysisDTO dataItem)  {
+        BranchAnalysisVO vo = branchAnalysisService.getBranchAnalysisService(dataItem);
+        return success(vo);
+    }
+}
+
---
+++ zhitan-system/src/main/java/com/zhitan/branchanalysis/service/IBranchAnalysisService.java
@@ -0,0 +1,16 @@
+package com.zhitan.branchanalysis.service;
+
+
+import com.zhitan.branchanalysis.domain.BranchAnalysisVO;
+import com.zhitan.realtimedata.domain.dto.BranchAnalysisDTO;
+
+
+public interface IBranchAnalysisService {
+    /**
+     * 支路用能分析
+     *
+     * @author sys
+     * @date 2021-01-11
+     */
+    BranchAnalysisVO getBranchAnalysisService(BranchAnalysisDTO dataItem);
+}
